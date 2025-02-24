7dc197ba9cf77a033ad29f80782800f4051bd9bf
Joan González <joangonzalezalbert@gmail.com>
2025-02-24 13:52:34 +0100

    Delete firebase

--- src/Pages/Portofolio.jsx
+++ src/Pages/Portofolio.jsx
@@ -15,6 +15,7 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import Certificate from "../components/Certificate";
 import { Code, Award, Boxes } from "lucide-react";
+import { certificateData, projectData } from "../services";
 
 // Separate ShowMore/ShowLess button component
 const ToggleButton = ({ onClick, isShowingMore }) => (
@@ -134,22 +135,8 @@ export default function FullWidthTabs() {
 
   const fetchData = useCallback(async () => {
     try {
-      const projectCollection = collection(db, "projects");
-      const certificateCollection = collection(db, "certificates");
 
-      const [projectSnapshot, certificateSnapshot] = await Promise.all([
-        getDocs(projectCollection),
-        getDocs(certificateCollection),
-      ]);
-
-      const projectData = projectSnapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-        TechStack: doc.data().TechStack || [],
-      }));
-
-      const certificateData = certificateSnapshot.docs.map((doc) => doc.data());
-
+      // TODO -> backend
       setProjects(projectData);
       setCertificates(certificateData);
 
---
+++ src/services/index.js
@@ -0,0 +1,277 @@
+const certificateData = [
+    {
+        "Img": "https://lh3.googleusercontent.com/pw/AP1GczOHjlE-lvr8njsY8pa2lxoPH8FzFr-4FNnjAMU1ttlTa0X6ubIVYga1cCmKP__rhF2asOzbZxbJeR-OKqwjmH3jde1rL2Bl6wcAJZEsN04BJ9AXAqAiV5bkm1tWzlrdfWgpgvrBFgHh76l8_KcjskBZ=w961-h679-s-no-gm?authuser=0"
+    },
+    {
+        "Img": "https://lh3.googleusercontent.com/pw/AP1GczN0V92mshe0GcjgjMn3g116zaOGTto7lXMKDOsFuphO3Iey6tZVTu6juSYAuCx5kjDb_YKZbNBTlPayw8s0749Qt-MiAFhedZBz0OC2CZnyxzcDsM3TAtectNWbKgHGlr1pEXylAB5ej4oldHQJh1hQ=w960-h679-s-no-gm?authuser=0"
+    },
+    {
+        "Img": "https://lh3.googleusercontent.com/pw/AP1GczOYtlztmqMHcYdUepBFDvWKxCBuWFH90xCle0eumBdIK4nm89udHcyYCWPDxk6OLtZmAp6K1vTP9zcuBHk_xW8e46805NoptUjh9F3ClcKLe_-ps32mTnxFoIiDCtclgU-FgWyz5cfg_fM5Q7FkDbBQ=w961-h679-s-no-gm?authuser=0"
+    },
+    {
+        "Img": "https://lh3.googleusercontent.com/pw/AP1GczMyc91Eh9_Nz5q9d5Ty1G_UTqIghhWrHbTXTGrBMZcriQkIxQYih0oXuj6YtufalRaZCwhcCLtuzrOqOx-t-czrq4DOJSFKfl_qbmGX7D8wejLonmA8EdrYL7cCMZOOEmIKLXAFNus9SkWAs5wZHR6P=w961-h679-s-no-gm?authuser=0"
+    },
+    {
+        "Img": "https://lh3.googleusercontent.com/pw/AP1GczMi-peaFb_Pd9JSx34i-nnslx8yk8n2urfjdsgRKBxtexSpj8T4UZAVKX5s_7KtOYt8TpWru1Px1FzJFapc6NjG3FUskuvfQ42FITkWDje0cI_M9hGL5Hi0yN2wA7tZZjO6gSoGRkDKx6th_Opo6W6A=w960-h679-s-no-gm?authuser=0"
+    },
+    {
+        "Img": "https://lh3.googleusercontent.com/pw/AP1GczMkvr4um7-5wWWIuZl0k9OX5N0VsejKCC6tsmqGUnIAH0IjI8vT_n4OqMhybSs6B9u8EO383ruBB8SFN-pbQkRCdiNkealgvpWNPdBEtGiowCBhYN6myUoqd1T_808bvM2ozPT5p_c8the-ce28BTGt=w960-h679-s-no-gm?authuser=0"
+    },
+    {
+        "Img": "https://lh3.googleusercontent.com/pw/AP1GczMTij3hIlw0e1bST38aaNw44JDgQtSPewEfefMGQIzCY8YHtNwPevJ-5qxuhmr_IRvqTRlMH3bNVFuup4NDippZtNJ5A25gzMARsoq4THhSwq-muRSiuigTRPk41z-Uo1wzmhDfUki21QoQ4Dw5kT75=w959-h679-s-no-gm?authuser=0"
+    }
+]
+
+const projectData = [
+    {
+        "id": "Aritmatika Solver",
+        "Link": "https://replit.com/@EkiZR/Aritmatika-Project",
+        "Github": "https://github.com/EkiZR/Aritmatika-Solver",
+        "Title": "Aritmatika Solver",
+        "Features": [
+            "Menghitung suku tertentu dari barisan aritmatika dengan menggunakan rumus suku ke-n.",
+            "Menentukan suku pertama atau beda jika hanya dua suku diketahui dalam barisan aritmatika.",
+            "Menghitung jumlah n suku pertama dari deret aritmatika dengan langkah perhitungan yang jelas."
+        ],
+        "Img": "https://lh3.googleusercontent.com/pw/AP1GczN8nCgz3uDAYTz959bL9wjcgXhPsT2-czigri6o-HkkyyIJ2uEagCkxFUsJtdcFFKoSblBWf0VUi_UY2bgrcHCmgG-cFxBz8zH7CM9AM0B6YpxmkMJ1gmL6dGSzzb0efVgKBcnDg_4eWmhmv0TSypNx=w1326-h679-s-no-gm?authuser=0",
+        "TechStack": [
+            "Python"
+        ],
+        "Description": "Program ini dirancang untuk mempermudah pengguna dalam menyelesaikan soal-soal Aritmatika secara otomatis dengan menggunakan bahasa pemrograman Python. Tujuan utama dari program ini adalah untuk membantu pengguna, terutama pelajar, dalam menyelesaikan soal-soal Aritmatika dengan lebih cepat dan mudah"
+    },
+    {
+        "id": "AutoChat-Discord",
+        "Github": "Private",
+        "Link": "https://autochat.my.id/",
+        "TechStack": [
+            "ReactJS",
+            "AOS",
+            "Tailwind CSS",
+            "Material UI",
+            "Python",
+            "Firebase",
+            "Sweetalert2",
+            "Vite",
+            "CSS"
+        ],
+        "Title": "AutoChat-Discord",
+        "Description": "AutoChat adalah solusi otomatisasi untuk mengirim pesan ke saluran Discord secara terjadwal. Pengguna dapat menentukan saluran tujuan, isi pesan, dan interval penundaan pengiriman pesan. Program ini berjalan 24/7, memungkinkan pengiriman pesan otomatis tanpa intervensi manual, sehingga memudahkan promosi atau komunikasi di Discord secara efisien.",
+        "Features": [
+            "Customize the message content as needed.",
+            "Send messages to multiple Discord channels simultaneously.",
+            "Set custom delay intervals between messages for controlled messaging.",
+            "AutoChat runs non-stop to ensure continuous messaging."
+        ],
+        "Img": "https://lh3.googleusercontent.com/pw/AP1GczO_D4RyZJmuV6r6cm1-gD4VURTIzUUvBa5JAbV7On4tJ60rL5KCvn-5QWIlZ2McjdcJ75QaZJdgM79d2_5XfEVQC_D1XCJZkh7pKX4aPLHzRnI2SsywyzU7NaapNLcCwrMjuHO3CD899cIgQGtizeQj=w1920-h983-s-no-gm"
+    },
+    {
+        "id": "Buku Catatan",
+        "TechStack": [
+            "ReactJS",
+            "Vite",
+            "CSS"
+        ],
+        "Features": [
+            "Pengguna dapat membuat, mengedit, dan mengatur catatan dengan mudah sesuai kebutuhan.",
+            "Memungkinkan pengguna untuk menemukan catatan dengan cepat menggunakan kata kunci."
+        ],
+        "Github": "ekizr.vercel.app",
+        "Img": "https://lh3.googleusercontent.com/pw/AP1GczM9nypDGzHROBamkpFL4a4H0w9LDD4FEvC1rPTRcUPunmWp6VTHwxrh88soMRZkTClx1bEEH7e9GZvjzltP5Pij4P4ou2e3wTvgIbggRxxdghrLfb5xuJnUiio_aZ6EHM5oF70Rv6ZbfsgXwPibBgrM=w1326-h679-s-no-gm?authuser=0",
+        "Link": "https://github.com/EkiZR/Buku-Catatan",
+        "Description": "Buku Catatan adalah website yang memungkinkan pengguna untuk membuat, menyimpan, dan mengelola catatan secara digital. Dengan antarmuka yang sederhana, pengguna dapat menulis, mengedit, dan mengakses catatan mereka kapan saja. Fitur pencarian juga memudahkan dalam menemukan catatan dengan cepat, menjadikannya solusi praktis untuk menyimpan ide dan informasi penting.",
+        "Title": "Buku Catatan"
+    },
+    {
+        "id": "Growtopia-Calculator",
+        "Description": "Growotopia Surgery Shop Calculator membantu pemain Growtopia menghitung keuntungan dari penjualan tools di vending shop surgery. Cukup masukkan jumlah pack dan harga per pack untuk menghitung modal, profit kotor, dan profit bersih, sehingga memudahkan perencanaan dan strategi penjualan.",
+        "TechStack": [
+            "GSAP",
+            "AOS",
+            "HTML",
+            "CSS",
+            "Javascript"
+        ],
+        "Img": "https://lh3.googleusercontent.com/pw/AP1GczNqjVfSWMu1M2XRNLeL3KM7soqBzsarT1lhi3rQWyKx6yp5YfK-nJhcdrsy2e-Dim1YtF0qwltbhY8y8RxZAADymL7Tjd2vtmI8gQIDBdrfcxkXf07Y00sCs-8c4Gq-Oq4l_HgiQk2vkmCk9nTP9_IM=w1920-h983-s-no-gm?authuser=0",
+        "Features": [
+            "Menghitung modal awal, profit kotor, dan profit bersih secara otomatis dari penjualan tools.",
+            "Mensimulasikan jumlah pack yang dijual dan harga per pack untuk merencanakan strategi penjualan.",
+            "Memberikan ringkasan data penjualan untuk membantu analisis performa toko vending."
+        ],
+        "Link": "https://growtopiacalculator.vercel.app/",
+        "Github": "https://github.com/EkiZR/Growtopia-Surgery-Shop-Calculator",
+        "Title": "Growtopia-Calculator"
+    },
+    {
+        "id": "IT Support Bekasi",
+        "Title": "IT Support Bekasi",
+        "Link": "https://itsupportbekasi.vercel.app/",
+        "Description": "Website IT Support Bekasi adalah proyek yang saya buat atas permintaan guru di sekolah, untuk menyediakan layanan IT di Bekasi. IT Support Bekasi adalah penyedia layanan terpercaya di bidang teknologi informasi, yang menawarkan service, maintenance, dan instalasi CCTV, serta pembuatan website yang menarik dan fungsional.",
+        "TechStack": [
+            "ReactJS",
+            "Vite",
+            "Tailwind CSS",
+            "AOS",
+            "CSS"
+        ],
+        "Github": "Private",
+        "Features": [
+            "Menawarkan jasa layanan IT seperti service, maintenance, dan instalasi CCTV.",
+            "Menampilkan galeri proyek yang telah dikerjakan.",
+            "Fitur kontak untuk mempermudah konsultasi dan permintaan layanan."
+        ],
+        "Img": "https://lh3.googleusercontent.com/pw/AP1GczMtrTVZHpOJtTEMxAnbQ_pD9QEQxt2maTjN8i1-AtvZgXJMwoccsf_oT-GHNu4WXtLivzu7I_otrvoGWdMRhCeuHIQIJ-nVWuU4aU2UJvRpztUL44_ffHthI8lkoHGI_xSo5XXKtVKOHpTd_pRXnSP3=w1723-h882-s-no-gm?authuser=0"
+    },
+    {
+        "id": "Oprec24",
+        "Link": "https://oprec24.my.id/",
+        "Title": "Oprec 24",
+        "Description": "Sebuah project pembuatan website organisasi yang mencakup deskripsi, galeri foto, dan profil anggota. Website ini berfungsi sebagai dokumentasi digital untuk menyimpan dan membagikan kenangan berharga yang pernah ada, agar tetap bisa dikenang dan diakses kapan saja.",
+        "Img": "https://lh3.googleusercontent.com/pw/AP1GczOQqAUVVH_v0AG6o0zggVyrfhh4mhB6gLwxocCZSVQz7WmLZnT5TNJp1Sqav7FUqAGT_BuiXNcCrmchTqvxVRVMy2i1XcT2s7qf0UmFiKwrTPdiDS6vkZ-2wg6W9BBGVH1nEM7_Zdsv5nzuG82LgP8y=w1920-h983-s-no-gm?authuser=0",
+        "TechStack": [
+            "ReactJS",
+            "Framer-Motion",
+            "Lucide-React",
+            "Tailwind CSS",
+            "AOS"
+        ],
+        "Features": [
+            "Menampilkan galeri",
+            "Fitur Komentar"
+        ],
+        "Github": "Private"
+    },
+    {
+        "id": "Portofolio-V4",
+        "TechStack": [
+            "ReactJS",
+            "MUI",
+            "Tailwind CSS",
+            "Vercel",
+            "AOS"
+        ],
+        "Title": "Portofolio-V4",
+        "Img": "https://lh3.googleusercontent.com/pw/AP1GczNd83RIs78HOkEaKVc-fHyOsIbRHZcx4cfngGL5mcJVRqPAaX91dLtZvSrkXMHiccb_C4_iJykqaG3rjrZ0uJ8b57tyTdWAVzYvkxJLbMQbvMmiw6IMOYNM1cW1ZyKQ20UwtKunsrKyk5pp9Ip9O5h_=w1920-h983-s-no-gm?authuser=0",
+        "Link": "https://ekizr-v4.vercel.app/",
+        "Github": "https://github.com/EkiZR/Portofolio-V4",
+        "Description": "Website Portofolio-v4 adalah versi sebelumnya dari website ini yang berisi informasi mengenai berbagai proyek yang telah saya kerjakan, sertifikasi yang saya peroleh, serta teknologi yang saya kuasai. ",
+        "Features": [
+            "Menampilkan berbagai proyek yang telah diselesaikan dengan berbagai detail informasi",
+            "Menampilkan sertifikat yang saya peroleh",
+            "Menampilkan teknologi yang di pakai di setiap proyek"
+        ]
+    },
+    {
+        "id": "QR Code Generator",
+        "Title": "QR Code Generator",
+        "Img": "https://lh3.googleusercontent.com/pw/AP1GczPR9Az7X8J7dXmobR47qDCfqPwpWqV6g2-UleDxG5hTXA_km0NZlxl4KwmtXahDgaZkY_Nfvad2HFmndy57z-c2AWTzo1_s4uP1bykFUhB-ou8CNHW8mWpg6geSxhG6DVQnvFKufkIMfVae45kzr1O3=w1326-h679-s-no-gm?authuser=0",
+        "TechStack": [
+            "Popperjs",
+            "Javascript",
+            "HTML",
+            "CSS"
+        ],
+        "Features": [
+            "Mengubah teks atau URL menjadi QR Code dengan mudah.",
+            "Menyediakan opsi untuk mengunduh QR Code yang telah dibuat."
+        ],
+        "Github": "https://github.com/EkiZR/QR-Code-Generator",
+        "Link": "https://qrcreatemagic.vercel.app/",
+        "Description": "QR Code Generator adalah website yang memungkinkan pengguna membuat QR Code dari teks atau link yang dimasukkan. Pengguna dapat dengan mudah menghasilkan kode QR untuk berbagi informasi secara cepat dan efisien."
+    },
+    {
+        "id": "The-Cats",
+        "Description": "Proyek ini adalah pembuatan website galeri kucing interaktif yang memungkinkan pengguna untuk mengunggah dan berbagi foto kucing. Dengan antarmuka yang sederhana, pengguna dapat dengan mudah menampilkan dan mengeksplorasi koleksi foto kucing yang diunggah oleh pengguna lain.",
+        "TechStack": [
+            "ReactJS",
+            "Vite",
+            "Tailwind CSS",
+            "Sweetalert2",
+            "Firebase",
+            "Material UI",
+            "CSS"
+        ],
+        "Img": "https://lh3.googleusercontent.com/pw/AP1GczP7--4BuY7jBl-o1gxq3ULh5x_CdoEo4OqrqP7woR4hdzmCxIyNkCHv-uzNqRZ82za5h4iLzZvBPLMRFjckphJ5-KkRw_t0qdXR843pdUu8x9iWwKCRZM2w3AG4HpzG2Mj6Xg33dfS1_D60ERXIQvyZ=w1317-h679-s-no-gm?authuser=0",
+        "Github": "https://github.com/EkiZR/Project-The-Cats",
+        "Link": "https://thecats.vercel.app/",
+        "Features": [
+            "Melihat koleksi foto kucing yang dibagikan oleh orang lain.",
+            "Mengunggah foto kucing Anda untuk dibagikan dengan orang lain."
+        ],
+        "Title": "The Cats"
+    },
+    {
+        "id": "Web Kelas V1",
+        "Features": [
+            "Menyediakan jadwal pelajaran yang memudahkan siswa mengakses informasi terkait kelas.",
+            "Menampilkan jadwal piket untuk membantu siswa mengatur tugas dan tanggung jawab.",
+            "Fitur menfess yang memungkinkan siswa berbagi pesan secara anonim dengan teman-teman mereka."
+        ],
+        "Github": "Private",
+        "Img": "https://lh3.googleusercontent.com/pw/AP1GczPUL5zRIvV23mf1OlLsvpgVtTrTVOjiMFaWfKKJsPonba-F3Ei0dULsHYz9M0SqoqRIWDpLXHIShpIk1yFKwWv0DmPCeeA0IwbWDMsRvE_LaPY8HxmjcDkHtP0Z2tfUM8CD2VdUuXS6_kSsXIVH6oSg=w1326-h679-s-no-gm?authuser=0",
+        "TechStack": [
+            "HTML",
+            "CSS",
+            "Javascript",
+            "Boostrap",
+            "AOS"
+        ],
+        "Title": "Web Kelas V1",
+        "Description": "Website Kelas v1 adalah proyek web pertama yang saya kerjakan bersama teman saya. bertujuan untuk menyediakan pusat informasi interaktif bagi siswa, dengan fitur jadwal pelajaran, jadwal piket, dan menfess, yang memudahkan siswa dalam mengakses informasi terkait kegiatan belajar mereka.",
+        "Link": "https://x-tkj-3.vercel.app/"
+    },
+    {
+        "id": "Web Kelas V2",
+        "Features": [
+            "Menyediakan fitur Text Anonim untuk berbagi pesan anonim antar siswa.",
+            "Menyediakan Gallery untuk berbagi foto-foto kegiatan kelas.",
+            "Menyediakan Schedule yang memudahkan siswa mengakses jadwal pelajaran dan kegiatan kelas.",
+            "Menyediakan Structure yang menampilkan informasi terkait struktur dan organisasi kelas."
+        ],
+        "TechStack": [
+            "Firebase",
+            "ReactJS",
+            "CSS",
+            "Material UI",
+            "Sweetalert2",
+            "Slick",
+            "Tailwind CSS",
+            "AOS",
+            "Vite"
+        ],
+        "Img": "https://lh3.googleusercontent.com/pw/AP1GczPOasnWRl6T-UawHGJs7hMU2ZcMCsP95pKAYKHJgTLRzNJs4Hsx8XHVMJHS9d25RxaPsmFxDho1Tnqe9HwcfMdBSiz2WKUtvxuja0P_usOo9rJJyHaLQm-zOvteoDdgYH003WxSOvgUkG31mRnXMWm5=w1326-h679-s-no-gm?authuser=0",
+        "Link": "https://xitkj3.vercel.app/",
+        "Description": "Website Kelas v2 adalah proyek yang saya kerjakan bersama teman, bertujuan untuk menyediakan informasi seputar kelas dengan fitur utama seperti Text Anonim, Gallery, Schedule, dan Structure. Website ini memudahkan siswa dalam mengakses informasi terkait kegiatan kelas.  Proyek ini juga berhasil meraih posisi Top 1 SEO untuk kata kunci \"XI TKJ 3\" dan \"TKJ 3\", meningkatkan visibilitas dan mempermudah akses informasi terkait kelas.",
+        "Github": "https://github.com/EkiZR/Web-Kelas-V2",
+        "Title": "Web Kelas V2"
+    },
+    {
+        "id": "WhatsApp Clone",
+        "Link": "https://ekizr.github.io/WhatsClone/whatsapp.html",
+        "Description": "Proyek ini adalah replika visual dari antarmuka aplikasi WhatsApp, yang dibangun menggunakan HTML, CSS, dan JavaScript. Proyek ini bertujuan untuk meniru desain dan tampilan WhatsApp secara mendetail serta memberikan saya wawasan dalam pengembangan antarmuka web.",
+        "Img": "https://lh3.googleusercontent.com/pw/AP1GczO321A1J5waLZu9cHI4SM30wi1iKspuwHWJZ7eojb6Npb8qyxdmCrjCE0XgwbUPw-pRFlidDsCu62ThCjj2saEHU5jnSX0OBo1AORToXUX93Q52ZGpq1bLFuos_73EtJKsL4C-iQ1ehnlyvqUlRSJAD=w1326-h679-s-no-gm?authuser=0",
+        "Github": "https://github.com/EkiZR/WhatsClone",
+        "Title": "WhatsApp Clone",
+        "TechStack": [
+            "HTML",
+            "CSS",
+            "Javascript",
+            "Bootstrap",
+            "SweetAlert2"
+        ],
+        "Features": [
+            "Mengganti judul halaman sesuai keinginan pengguna.",
+            "Dapat mengubah nama kontak",
+            "Mengedit pesan yang sudah dikirimkan.",
+            "Menyesuaikan waktu pengiriman pesan.",
+            "Mengubah status tanda centang (single, double, atau centang biru).",
+            "Memungkinkan tampilan dalam mode layar penuh (Full Screen).",
+            "Menyediakan opsi Mode Gelap dan Mode Terang untuk kenyamanan visual.",
+            "Dapat mengunggah status"
+        ]
+    }
+]
+
+export {
+    projectData,
+    certificateData
+}
